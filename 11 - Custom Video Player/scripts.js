d8250fcbefcc2181d41a9c773132e62c513e612c
Sadie109 <101196558+Sadie109@users.noreply.github.com>
2022-12-12 17:01:19 +0000

    built toggleplay function and added eventlistener to buttons and toggle

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,7 +1,21 @@
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.play__slider');
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+function togglePlay() {
+  // other option to use tenerary operator
+  // const method = video.paused ? 'play' : 'paused';
+  // video[method]();
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
